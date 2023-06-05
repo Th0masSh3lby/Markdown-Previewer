b5bb07ed24d07e19f99ea14d49487c82e540d4a8
Aravind Reddy Choppa <choppaaravind@gmail.com>
2023-06-05 14:05:08 +0530

    completed Markdown-previewer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { marked } from "marked";
 
 function App() {
+  const [current, Setcurrent] = useState(true);
+  const [button, Setbutton] = useState("x");
+
+  function toggle(event) {
+    if (current == true) {
+      Setcurrent(false);
+      Setbutton("-");
+    } else {
+      Setcurrent(true);
+      Setbutton("x");
+    }
+    return;
+  }
+
+  const [text, setText] = useState(`
+  # Heading
+  ## SubHeading
+  ### h3
+  [Title(website-link)](https://www.example.com)
+  ![alt text](image.jpg)
+  
+  - First item
+    - Second item
+      - Third item
+      
+      
+      
+  \` single tick for single line code\`
+
+
+  \`\`\`
+      {
+        triple tick for multiline code
+      }
+   \`\`\`
+   > blockquote
+   **bold text**
+       `);
+
+  marked.setOptions({
+    breaks: true,
+  });
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="textheader">
+        <p className="Thead">Editor</p>
+        <button onClick={toggle}>{button}</button>
+      </div>
+      <textarea
+        className={current ? "editor" : "editorfull"}
+        id="editor"
+        onChange={handleChange}
+        value={text}
+      ></textarea>
+
+      <div className={current ? "previewheader" : "previewheaderclose"}>
+        <p className="Thead">Previewer</p>
+      </div>
+      <div
+        className={current ? "preview" : "previewclose"}
+        id="preview"
+        dangerouslySetInnerHTML={{ __html: marked(text) }}
+      ></div>
     </div>
   );
 }
